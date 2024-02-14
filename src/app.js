15bb05cd91e0fb215d99018975cea076375f2a91
Manuel García <153007701+delafter@users.noreply.github.com>
2024-02-14 20:20:07 +0000

    generadorManu

--- src/app.js
+++ src/app.js
@@ -4,9 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let who = ["El perro", "Mi abu", "La tortuga", "Un pokemon"];
+let action = ["se comio", "se orino", "se aplasto", "se rompio"];
+let what = ["mis deberes", "mis llaves", "la tarjeta"];
+let when = [
+  "despues de las clases",
+  "en el momento",
+  "que estaba terminando",
+  "cuando me estaba duchando",
+  "...no te quiero mentir no me gustas"
+];
 
+function seleccionAlAzar(quien) {
+  return quien[Math.floor(Math.random() * quien.length)];
+}
 window.onload = function() {
   //write your code here
+  console.log("hola");
+  let miCaja = document.getElementById("miCaja");
+  miCaja.textContent =
+    seleccionAlAzar(who) +
+    " " +
+    seleccionAlAzar(action) +
+    " " +
+    seleccionAlAzar(what) +
+    " " +
+    seleccionAlAzar(when);
+
+  console.log(miCaja);
+  //let elemento = document.getElementById("exusa");
+
+  //function onLoad(){
+  //  var excusa = document.getElementById("excusa");
+  //excusa.innerHTML = seleccionAlAzar(quien, suceso, que, cuando);
+  //}
+  //window.onload = onLoad;
 };
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-function seleccionarDatosAlAzar(datos) {}
